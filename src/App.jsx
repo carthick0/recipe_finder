54ec0c4dfc84d1ba5b7ed22072d7bde0e03f984a
keya <karthikvempala31@gmail.com>
2025-01-23 12:20:15 +0530

    l

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
 
 import FavouritesPage from "./pages/FavouritesPage";
-import HomePage  from "./pages/Homepage";
+import HomePage from "./pages/Homepage";
 
 function App() {
   return (
